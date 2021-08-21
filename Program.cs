21e63635bd5ac1cfa6404cc569ab3788fcb1afe9
AnaComBia <anabeatrizetorres@gmail.com>
2021-08-21 15:16:11 -0300

    Finaliza Programa AsciiArt

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AsciiArt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(" ||  ||");
+            Console.WriteLine(" \\\\()//");
+            Console.WriteLine("//(__)\\\\");
+            Console.WriteLine("||     ||");
+            Console.ResetColor();
+
+
+        }
+    }
+}
